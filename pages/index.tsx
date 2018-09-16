234d72832ead8f5e2958a93f161afddef6783522
diox <markuswagner93@gmail.com>
2018-09-16 22:57:31 +0200

    enabled typescript compiling for the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import Layout from '../components/DefaultLayout.js';
+import Layout from '../components/DefaultLayout';
 import Link from 'next/link';
+import React from 'react';
 
 function getPosts() {
-	return [
-		{id: 'hello-nextjs', title: 'Hello Next.js'},
-		{id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
-		{id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
-	]
+  return [
+    {id: 'hello-nextjs', title: 'Hello Next.js'},
+    {id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+    {id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+  ]
 }
 
-const PostLink = ({post}) => (
-	<li>
-		<Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-			<a>{post.title}</a>
-		</Link>
-		<style jsx>{`
+const PostLink = ({key, post}) => (<li>
+  <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+    <a id={key}>{post.title}</a>
+  </Link>
+  <style jsx>{`
       li {
         list-style: none;
         margin: 5px 0;
@@ -30,18 +30,17 @@ const PostLink = ({post}) => (
         opacity: 0.6;
       }
     `}</style>
-	</li>
-)
+</li>);
 
 export default () => (
-	<Layout>
-		<h1>My Blog</h1>
-		<ul>
-			{getPosts().map((post) => (
-				<PostLink key={post.id} post={post}/>
-			))}
-		</ul>
-		<style jsx>{`
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post}/>
+      ))}
+    </ul>
+    <style jsx>{`
       h1, a {
         font-family: "Arial";
       }
@@ -64,5 +63,5 @@ export default () => (
         opacity: 0.6;
       }
     `}</style>
-	</Layout>
+  </Layout>
 )
