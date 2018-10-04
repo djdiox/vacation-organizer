003362d1f8702edb83b7e73db61d453865fd4dcc
mwagner <mwagner@netvico.com>
2018-10-04 14:37:40 +0200

    added jest setup for unit testing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withSass = require('@zeit/next-sass');
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript(withSass());
+const withTypescript = require('@zeit/next-typescript');
+module.exports = {
+  ...withTypescript(withSass()),
+  serverRuntimeConfig: { // Will only be available on the server side
+    mySecret: 'secret'
+  },
+  publicRuntimeConfig: { // Will be available on both server and client
+    staticFolder: '/static',
+    mySecret: process.env.MY_SECRET // Pass through env variables
+  }
+};
